c41a2b9faea689ec5b0c65a3a1fbf0b7a51d028a
Gilderko <martin.sepela007@gmail.com>
2021-07-14 14:07:02 +0200

    Unit Storing

---
+++ Assets/Scripts/Networking/RTSPlayer.cs
@@ -0,0 +1,110 @@
+using Mirror;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RTSPlayer : NetworkBehaviour
+{
+    [SerializeField]
+    private List<Unit> myUnits = new List<Unit>();
+
+    #region Server
+
+    [Server]
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+
+        Unit.ServerOnUnitSpawned += ServerHandleUnitSpawned;
+        Unit.ServerOnUnitDespawned += ServerHandleUnitDespawned;
+    }    
+
+    [Server]
+    private void ServerHandleUnitSpawned(Unit unit)
+    {
+        // Check if the same person who owns this player also owns this unit
+        if (unit.connectionToClient.connectionId != connectionToClient.connectionId)
+        {
+            return;
+        }
+
+        myUnits.Add(unit);
+    }
+
+    [Server]
+    public override void OnStopServer()
+    {
+        base.OnStopServer();
+
+        Unit.ServerOnUnitSpawned -= ServerHandleUnitSpawned;
+        Unit.ServerOnUnitDespawned -= ServerHandleUnitDespawned;
+    }
+
+    [Server]
+    private void ServerHandleUnitDespawned(Unit unit)
+    {
+        if (unit.connectionToClient.connectionId != connectionToClient.connectionId)
+        {
+            return;
+        }
+
+        myUnits.Remove(unit);
+    }
+
+    #endregion
+
+    #region Client
+
+    [Client]
+    public override void OnStartClient()
+    {
+        if (!isClientOnly)
+        {
+            return;
+        }
+
+        base.OnStartClient();
+
+        Unit.AuthorityOnUnitSpawned += AuthorityHandleUnitSpawned;
+        Unit.AuthorityOnUnitDespawned += AuthorityHandleUnitDespawned;
+    }
+
+    [Client]
+    public override void OnStopClient()
+    {
+        if (!isClientOnly)
+        {
+            return;
+        }
+
+        base.OnStartClient();
+
+        Unit.AuthorityOnUnitSpawned -= AuthorityHandleUnitSpawned;
+        Unit.AuthorityOnUnitDespawned -= AuthorityHandleUnitDespawned;
+    }
+
+    [Client]
+    private void AuthorityHandleUnitSpawned(Unit unit)
+    {
+        if (!hasAuthority)
+        {
+            return;
+        }
+
+        myUnits.Add(unit);
+    }
+
+    [Client]
+    private void AuthorityHandleUnitDespawned(Unit unit)
+    {
+        if (!hasAuthority)
+        {
+            return;
+        }
+
+        myUnits.Remove(unit);
+    }
+
+    #endregion
+}
--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -3,16 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 using UnityEngine.Events;
+using System;
 
 public class Unit : NetworkBehaviour
 {
     [SerializeField] private UnityEvent onSelected = null;
-
     [SerializeField] private UnityEvent onDeselected = null;
-
     [SerializeField] private UnitMovement unitMovement = null;
 
+    public static event Action<Unit> ServerOnUnitSpawned;
+    public static event Action<Unit> ServerOnUnitDespawned;
+
+    public static event Action<Unit> AuthorityOnUnitSpawned;
+    public static event Action<Unit> AuthorityOnUnitDespawned;
+
+    #region Server
+
+    [Server]
+    public override void OnStartServer()
+    {
+        ServerOnUnitSpawned(this);
+    }
+
+    [Server]
+    public override void OnStopServer()
+    {
+        ServerOnUnitDespawned(this);
+    }
+
+    #endregion
+
     #region Client
+
+    [Client]
+    public override void OnStartClient()
+    {
+        if (!hasAuthority || !isClientOnly)
+        {
+            return;
+        }
+
+        AuthorityOnUnitSpawned(this);
+    }
+
+    [Client]
+    public override void OnStopClient()
+    {
+        if (!hasAuthority || !isClientOnly)
+        {
+            return;
+        }
+
+        AuthorityOnUnitDespawned(this);
+    }
 
     [Client]
     public UnitMovement GetUnitMovement()
@@ -40,11 +83,7 @@ public class Unit : NetworkBehaviour
         }
 
         onDeselected.Invoke();
-    }
-
-    #endregion
-
-    #region Server
+    }  
 
     #endregion
 }
